24ba5038a18bf727344ca122a1a7339da3b6b2d8
andrit <arnewmediatech@gmail.com>
2018-07-11 22:48:45 -0400

    getting late back to working form...maybe work on that state flag issue at another time

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,24 +1,11 @@
 import React, {Component} from 'react';
-import {Modal} from '../resultsmodal';
 import './results.css';
 
 /*export const Results = (props) => {
     return()
 }*/
 class Results extends Component{
-    state={
-        showModal: false
-    }
-    handleOpenModal = () => {
-        this.setState({
-            showModal: true
-        })
-    }
-    handleCloseModal = () => {
-        this.setState({
-            showModal: false
-        })
-    }
+
     render(){
         const {searchresults} = this.props;
         const ending = '...';
@@ -27,19 +14,14 @@ class Results extends Component{
             <div id="albums-container">
                 {searchresults.map((album, i) => {
                     return(
-                        <React.Fragment>
-                        <div className="single-album" onClick={this.handleOpenModal} key={i}>
+                        <div className="single-album" key={i}>
                             <img src={album.artworkUrl100} alt={album.collectionName} />
                             <h3>{album.collectionName.length > length 
                                 ? album.collectionName.substring(0, length - ending.length) + ending
                                 : album.collectionName
                                 }</h3>
                         </div>
-                        
-                        {this.state.showModal
-                            ?  <Modal closeModal={this.handleCloseModal} artistpic={album.artistViewUrl} name={album.artistName} genre={album.primaryGenreName} track={album.trackName} album={album.collectionName} />
-                            : <br/>}
-                        </React.Fragment>
+
                     );
                 })}
             </div>
